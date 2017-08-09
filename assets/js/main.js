201afc1043d8fda7307fe0ed909d61eeb7894670
Hendrix Roa <hendrix.alex87@gmail.com>
2017-08-09 11:44:42 -0400

    arreglo de los estilos

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,9 @@
 "use strict";
 
 var Main = ( function(){
-
     window.onload = function() {
         document.querySelectorAll('.level-bar-inner').forEach((item) => {
             item.style.width = item.dataset.level;
         });
     };
-
 })();

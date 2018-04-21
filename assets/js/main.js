242a4d098c60fda3f377e1991e70214deb9db411
Hendrix Roa <hendrix.alex87@gmail.com>
2018-04-20 21:34:45 -0500

    agregado handleblars

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,298 @@
 "use strict";
 
+var dataEs = {
+    bio: "Me considero un joven entusiasta por la tecnología y apasionado por la programación de software de calidad que brinden la satisfacción y el mayor desempeño según las exigencias del cliente. Desde los inicios de mi formación en el área de la tecnología he notado como la misma crece y cambia a un ritmo vertiginoso y es ahí donde debemos preguntarnos si seguir creciendo a la par con ella o dedicarnos a otra cosa. Yo opto por seguir creciendo y dar un servicio de calidad.",
+    description: "Desarrollador Backend con nociones de Frontend",
+    address: "Lima-Perú",
+    personalInfo: [
+        {
+            icon: "fa fa-envelope",
+            info: "hendrix.alex87@gmail.com",
+            name: "hendrix.alex87@gmail.com"
+        },
+        {
+            icon: "fa fa-phone",
+            info: "+51982544588",
+            name: "+51982544588"
+        },
+        {
+            icon: "fa fa-globe",
+            info: "https://hendrixroa.blogspot.com",
+            name: "Blog"
+        },
+        {
+            icon: "fa fa-linkedin",
+            info: "https://www.linkedin.com/in/hendrix-roa-a6991779/",
+            name: "Linkedin"
+        },
+        {
+            icon: "fa fa-github",
+            info: "https://github.com/hendrixroa",
+            name: "https://github.com/hendrixroa"
+        },
+        {
+            icon: "fa fa-twitter",
+            info: "https://twitter.com/hendrix_roa",
+            name: "@hendrix_roa"
+        },
+        {
+            icon: "fa fa-instagram",
+            info: "@hendrixroa",
+            name: "hendrixroa"
+        }
+    ],
+    titleEducation: "Educación",
+    education: [
+        {
+            title: "Ciencias de la Computación",
+            name: "Univesidad Central de Venezuela",
+            period: "2011 - 2017 (No culminados)"
+        }
+    ],
+    titleLanguages: "Idiomas",
+    laguanges:[
+        {
+            name: "Español",
+            desc: "(Nativo)"
+        },
+        {
+            name: "Ingles",
+            desc: "(Técnico)"
+        }
+    ],
+    titleInterest: "Intereses",
+    intetest:[
+        "Programación funcional",
+        "Paradigmas de programación",
+        "Principios SOLID, YAGNI, DRY",
+        "Progressive Wep apps",
+        "Desarrollo de videojuegos",
+        "Unity game engine",
+        "Performance Apps",
+        "Refactorización de Código",
+        "Arquitectura de Software",
+        "Desarrollo Mobile Nativo",
+        "Aplicaciones de Escritorio",
+        "Soluciones tecnológicas aplicado a cualquier ámbito"
+    ],
+    titleExperiences: "EXPERIENCIAS",
+    experiences: [
+        {
+            title: "Desarrollador Backend",
+            period: "2017 - Actualidad",
+            company: "Stacksavings.com, California-Usa",
+            desc: "Trabajo en los distintos proyectos internos de la empresa, así como en los paquetes públicos de npm que en la empresa se desarrollan para aportar a la comunidad modulos para el manejo de criptomonedas. "
+        },
+        {
+            title: "Desarrollador C#/.net",
+            period: "2017 - 2018",
+            company: "ControlRoll.com, Chile",
+            desc: "Trabajé en los distintos portales de la empresa brindando nuevas funcionalidades y soporte. "
+        },
+        {
+            title: "Desarrollador FullStack",
+            period: "2017 - 2017",
+            company: "Shareable Innovations, Chile",
+            desc: "Trabajé en las funcionalidades de los portales que se desarrollaban, con un equipo de mas de 5 personas por proyecto. "
+        },
+        {
+            title: "Desarrollador FullStack",
+            period: "2016 - 2017",
+            company: "Remaster, Caracas",
+            desc: "Trabajé en los portales inmobiliarios de la empresa, brindando soporte y desarrollo de nuevas funcionalidades y mejoras en la experiencia de usuario. "
+        },
+        {
+            title: "Desarrollador FullStack",
+            period: "2016 - 2016",
+            company: "Socialh4ck, Caracas",
+            desc: "Trabajé en proyectos relacionados al marketing digital ofreciendo mejoras del servicio y experiencia de usuario. "
+        },
+        {
+            title: "Desarrollador Jr. & Analista de Soporte",
+            period: "2015 - 2016",
+            company: "4Geeks, Caracas",
+            desc: "Trabajé como analista de soporte en los proyectos desarrollados en la empresa, asi mismo atendía las exigencias del cliente directamente. "
+        },
+        {
+            title: "Pasante de Desarrollo",
+            period: "2015",
+            company: "4Geeks, Caracas",
+            desc: "Empecé desarrollando un módulo de gestión de servidores internos de la empresa. "
+        },
+        {
+            title: "Jr. Developer",
+            period: "2014 - 2015",
+            company: "Eficiatech, Caracas",
+            desc: "Empecé como pasante, aplicando los conocimientos de programación orientados a la web."
+        }
+    ],
+    titleColaborate: "Colaboraciones en Proyectos",
+    colaborate: [
+        {
+            name: "Golfitrion",
+            link: "https://golfonline.caracascountryclub.org/",
+            desc: "Un sitio para administrar partidas de golf"
+        },
+        {
+            name: "Pollito Ingles",
+            link: "http://www.pollitoingles.com/",
+            desc: "Un sitio para ventas e intercambios de articulos para bebes"
+        },
+        {
+            name: "Century 21 Peru",
+            link: "http://www.century21.pe/",
+            desc: "Sucursal de la franquicia Century21"
+        },
+        {
+            name: "Como quiero",
+            link: "http://comoquiero.cl/",
+            desc: "Un sitio para planificar recetas"
+        },
+        {
+            name: "Inmobilia",
+            link: "http://inmobilia.com/",
+            desc: "Portal innmobiliario de alquiler y venta"
+        },
+        {
+            name: "Remax Venezuela",
+            link: "http://remax.com.ve/",
+            desc: "Portal innmobiliario de alquiler y venta"
+        },
+        {
+            name: "Kronosocial",
+            link: "https://kronosocial.com/",
+            desc: "Planificador de redes sociales para community managers"
+        },
+        {
+            name: "Instacompras",
+            link: "https://instacompras.com/",
+            desc: "Ecommerce de productos varios"
+        },
+        {
+            name: "Beelivery",
+            link: "https://beelivery.cl/",
+            desc: "Sitio destinado a entregas"
+        },
+        {
+            name: "Cliente Poloniex",
+            link: "https://www.npmjs.com/package/@stacksavings/poloniex-client",
+            desc: "Paquete npm para el uso fácil de la api de poloniex"
+        },
+        {
+            name: "Cliente Aws-DynamoDB",
+            link: "https://www.npmjs.com/package/@stacksavings/dynamodb",
+            desc: " Paquete npm para el uso fácil de la api de AWS-DynamoDB"
+        },
+        {
+            name: "Uilidades comunes en proyectos de nodejs",
+            link: "https://www.npmjs.com/package/@stacksavings/utils",
+            desc: "Paquete para utilidades comunes en stacksavings."
+        }
+    ],
+    titlePersonalProjects: "Proyectos personales OpenSource",
+    personalProjects: [
+        {
+            name: "In case of fire",
+            link: "https://github.com/hendrixroa/in-case-of-fire",
+            desc: "Una herramienta para recordar subir cambios a Git"
+        }
+    ],
+    titleSkylls: "Habilidades",
+    skylls: [
+        {
+            name: "Python & Django",
+            level: "40%",
+        },
+        {
+            name: "React.js",
+            level: "50%",
+        },
+        {
+            name: "Javascript & Node Js",
+            level: "60%",
+        },
+        {
+            name: "Angular",
+            level: "40%",
+        },
+        {
+            name: "Electron Cross-plattforms develepment",
+            level: "35%",
+        },
+        {
+            name: "Php",
+            level: "55%",
+        },
+        {
+            name: "Php & MVC",
+            level: "50%",
+        },
+        {
+            name: "Java & Swing inteface",
+            level: "40%",
+        },
+        {
+            name: "HTML5 & CSS",
+            level: "45%",
+        },
+        {
+            name: "C & C++",
+            level: "50%",
+        },
+        {
+            name: "C#/aspx/.net",
+            level: "40%",
+        },
+        {
+            name: "Linux Desktop y Linux Sever",
+            level: "60%",
+        },
+        {
+            name: "Conocimientos en base de datos sql, tales como psql, mysql, mssql, sqlite",
+            level: "50%",
+        },
+        {
+            name: "Conocimientos en base de datos Nosql, tales como DynamoDB, MongoDB",
+            level: "30%",
+        },
+        {
+            name: "Ruby on Rails",
+            level: "40%",
+        },
+        {
+            name: "AWS Lambda",
+            level: "50%",
+        }
+    ]
+}
+
+
 var Main = ( function(){
-    window.onload = function() {
+    document.addEventListener("DOMContentLoaded", function(event) {
+        
+        /*var tmpl = Handlebars.compile(document.getElementById('cv').innerHTML);
+        var context = {};
+        context.dataEs = dataEs;
+        document.getElementById('cv').innerHTML = tmpl(context);*/
+
+        var source   = document.getElementById("some-template").innerHTML;
+
+        var template = Handlebars.compile(source);
+  
+        var data = { users: [
+  
+            {username: "alan", firstName: "Alan", lastName: "Johnson", email: "alan@test.com" },
+  
+            {username: "allison", firstName: "Allison", lastName: "House", email: "allison@test.com" },
+  
+            {username: "ryan", firstName: "Ryan", lastName: "Carson", email: "ryan@test.com" }
+  
+          ]};
+  
+        document.getElementById("content").innerHTML = template(data);
+  
         document.querySelectorAll('.level-bar-inner').forEach((item) => {
             item.style.width = item.dataset.level;
         });
-    };
+    });
 })();
